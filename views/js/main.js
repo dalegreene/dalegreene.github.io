6e8f611e9886288080c9d1364d079a35177b7c9d
Dale Greene <drgjr87@gmail.com>
2015-05-22 12:03:49 -0400

    P4 Update

--- views/js/main.js
+++ views/js/main.js
@@ -541,7 +541,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
 
     
-  for (var i = 0; i < 20; i++) { // DECREASED THE NUMBER OF PIZZAS TO DRAW ON SCREEN
+  for (var i = 0; i < 30; i++) { // DECREASED THE NUMBER OF PIZZAS TO DRAW ON SCREEN
     var newelem = elem.cloneNode(true); // CLONED ELEM NODE SO THAT THE APPENDCHILD CALL WOULD DRAW ALL PIZZAS
     newelem.basicLeft = (i % cols) * s;
     newelem.style.top = (Math.floor(i / cols) * s) + 'px';
