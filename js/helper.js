662c127ebe011bf844c7f1bafee57f654bc580ff
Dale Greene <drgjr87@gmail.com>
2015-11-19 03:19:57 -0500

    Resume Updates

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
@@ -20,14 +6,14 @@ var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<ol class="flex-item"><span class="white-text">%data%</span></ol>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
@@ -93,17 +79,9 @@ $(document).click(function(loc) {
 
 
 
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
+// Custom Google Map generation for the website.
 var map;    // declares a global map variable
 
-
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
 function initializeMap() {
 
   var locations;
@@ -112,15 +90,11 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
-  // <div id="map">, which is appended as part of an exercise late in the course.
+  // makes `map` a new Google Map JavaScript Object and attaches it to <div id="map">
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
+  // returns an array of every location string from the JSONs written for bio, education, and work.
   function locationFinder() {
 
     // initializes an empty array
@@ -129,14 +103,12 @@ function initializeMap() {
     // adds the single location property from bio to the locations array
     locations.push(bio.contacts.location);
 
-    // iterates through school locations and appends each location to
-    // the locations array
+    // iterates through school locations and appends each location to the locations array
     for (var school in education.schools) {
       locations.push(education.schools[school].location);
     }
 
-    // iterates through work locations and appends each location to
-    // the locations array
+    // iterates through work locations and appends each location to the locations array
     for (var job in work.jobs) {
       locations.push(work.jobs[job].location);
     }
@@ -144,11 +116,7 @@ function initializeMap() {
     return locations;
   }
 
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
+  // reads Google Places search results to create map pins
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
@@ -157,27 +125,23 @@ function initializeMap() {
     var name = placeData.formatted_address;   // name of the place from the place service
     var bounds = window.mapBounds;            // current boundaries of the map window
 
-    // marker is an object with additional data about the pin for a single location
+    // object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
       map: map,
       position: placeData.geometry.location,
       title: name
     });
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
+    // helper windows that open when you click or hover over a pin on a map
     var infoWindow = new google.maps.InfoWindow({
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
       infoWindow.open(map,marker);
     });
 
-    // this is where the pin actually gets added to the map.
+    // where the pin gets added to the map.
     // bounds.extend() takes in a map location object
     bounds.extend(new google.maps.LatLng(lat, lon));
     // fit the map to the new marker
@@ -186,27 +150,20 @@ function initializeMap() {
     map.setCenter(bounds.getCenter());
   }
 
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
+  // makes sure the search returned results for a location.
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
     }
   }
 
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
+  // takes in the array of locations created by locationFinder() and fires off Google place searches for each location
   function pinPoster(locations) {
 
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
+    // creates a Google place search service object
     var service = new google.maps.places.PlacesService(map);
 
-    // Iterates through the array of locations, creates a search object for each location
+    // iterates through the array of locations, creates a search object for each location
     for (var place in locations) {
 
       // the search request object
@@ -214,33 +171,26 @@ function initializeMap() {
         query: locations[place]
       };
 
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
+      // searches the Google Maps API for location data and runs the callback function with the search results after each search.
       service.textSearch(request, callback);
     }
   }
 
-  // Sets the boundaries of the map based on pin locations
+  // sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
 
-  // locations is an array of location strings returned from locationFinder()
+  // array of location strings returned from locationFinder()
   locations = locationFinder();
 
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
+  // creates pins on the map for each location in the locations array
   pinPoster(locations);
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
-// Calls the initializeMap() function when the page loads
+// calls the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
 
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
+// listens for resizing of the window and adjusts map bounds
 window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
  map.fitBounds(mapBounds);
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,17 +2,18 @@
 
 var bio = {
 	"name": "Dale Greene",
-	"role": "Front End Web Developer",
+	"role": "Front-End Web Developer",
 	"contacts": {
 		"mobile": "804-350-2908",
 		"email": "drgjr87@gmail.com",
 		"github": "dalegreene",
+		"linkedin": "dalegreene",
 		"location": "Richmond, VA"
 	},
 	"welcome": "Welcome to my resume!",
-	"skills": ["Front-End Web Development", "Computer Programming", "Process Management", "Peer Coaching"],
+	"skills": ["Java", "JavaScript", "JQuery", "CSS", "HTML", "HTML5", "C", "C++", "Unix", "Linux", "Assembly", "MATLAB", "QBasic", "Jasmine Tester", "GitHub", "Windows", "Mac OS", "Microsoft Office", "iWork"],
 	"picture": "images/profilepic.jpg",
-	"message": "Hello and thanks for viewing my resume!"
+	"message": "Hello, and thanks for viewing my resume!"
 }
 
 var work = {
@@ -23,11 +24,10 @@ var work = {
 			"dates": "January 2015-Present",
 			"location": "Richmond, VA",
 			"descriptions": [
-				"Assist on-phone agents with escalated concerns as well as manaage projects to improve processes within the department",
-				"Incorporate multi-tasking techniques to update and managed projects while assisting agents",
-				"Analyze department data to find trends in order to better coach agents to improve towards achieving department goals", 
-				"Facilitate development sessions to improve agents' performance",
-				"Collaborate with upper level management for process improvements"
+				"Assist phone agents & management with escalated issues on policies, procedures & systems to ensure proper processes & maximize customer convenience",
+				"Reduce departmental procedure errors monthly by researching accounts to find common trends and composing & facilitating help sessions to agents",
+				"Improve percentages of agents' customer satisfaction survey results by providing coaching feedback to phone agents & their corresponding managers",
+				"Conduct meetings with colleagues to collaborate on departmental trends in which initiatives are discussed towards statistical improvements"
 			]
 		},
 		{
@@ -36,11 +36,53 @@ var work = {
 			"dates": "January 2013-January 2015",
 			"location": "Richmond, VA",
 			"descriptions": [
-				"Handled incoming fraud calls and escalated calls from other groups, and used critical thinking skills to find the best solution for the customer",
-				"Worked in a fast-paced environment and quickly adjusted to policy changes and updates",
-				"Managed projects and facilitated presentations to enhance customer experience with other agents",
-				"Assisted in coaching and developing new onboarding agents",
-				"Earned ROAR Award honors in multiple yearly quarters for being among the top 5% in performance within the department"
+				"Maintained ability to multi-task in a fast-paced call center environment by servicing customers, using resources & re-viewing account activity",
+				"Investigated account activity to identify fraud, resolve discrepancies and properly notate & close cases",
+				"Helped improve department month to month CSAT scores by hosting call listening sessions with the purposes of agents seeking out best practices to incorporate within their own call flow on top of maintaining above average stats within primary job role",
+				"Honored as ROAR award recipient in numerous quarters - Ranked top 5% within department recognized for exceptional efforts in various statistical categories"
+			]
+		},
+		{
+			"position": "Receiving Associate",
+			"employer": "Bass Pro Shops",
+			"dates": "July 2012-July 2013",
+			"location": "Ashland, VA",
+			"descriptions": [
+				"Utilized efficient teamwork skills & maintained consistency to safely process freight received at a timely pace",
+				"Replenished & organized on-floor stock to maintain a pleasing floor presence for customers",
+				"Worked with management to introduce & implement new stock ideas periodically to improve neatness, accessibility & safety for back stocked & on-floor merchandise"
+			]
+		},
+		{
+			"position": "Sales Associate",
+			"employer": "Bass Pro Shops",
+			"dates": "August 2008-July 2012",
+			"location": "Ashland, VA",
+			"descriptions": [
+				"Maintained ability to perform various essential selling & customer service techniques, naturally greeting & acknowledging every customer to create a genuine connection",
+				"Attended STAR Luncheon monthly for consistently submitted customer compliments",
+				"Suggested floor setups that were implemented to increase potential sales for certain products"
+			]
+		},
+		{
+			"position": "Sales Associate",
+			"employer": "Dillard's",
+			"dates": "September 2007-August 2009",
+			"location": "Glen Allen, VA",
+			"descriptions": [
+				"Influenced returning business to store by building personal relationships with customers",
+				"Achieved top percentile in monthly sales every month by utilizing probing & suggestive selling techniques to discover customer needs"
+			]
+		},
+		{
+			"position": "Engineering Researcher",
+			"employer": "Research for Engineering Undergraduates",
+			"dates": "June 2007-August 2007",
+			"location": "Huntsville, AL",
+			"descriptions": [
+				"Researched at Alabama A&M University various characteristics of microelectromechanical systems sensors used for military security",
+				"Explored specific findings within a team environment which included observing the sensors’ functionality & determined accuracy readings based on distances away from home base",
+				"Collaboratively composed a 15-page research paper on findings & conducted a presentation on obtained research to military personnel"
 			]
 		}
 	]
@@ -49,28 +91,28 @@ var work = {
 var education = {
 	"schools": [
 		{
-			"name": "Montgomery College",
-			"location": "Rockville, MD",
-			"degree": "Associates in Sciences",
-			"majors": ["Electrical Engineering"],
-			"dates": 2012,
-			"url": "http://www.montgomerycollege.edu"
-		},
-		{
 			"name": "Virginia Commonwealth University",
 			"location": "Richmond, VA",
-			"degree": "Bachelor's",
+			"degree": "Bachelor of Science",
 			"majors": ["Electrical Engineering"],
 			"minor": "Math",
-			"dates": 2016,
+			"dates": "May 2017",
 			"url": "http://www.vcu.edu"
+		},
+		{
+			"name": "Montgomery College",
+			"location": "Rockville, MD",
+			"degree": "Associate of Science",
+			"majors": ["Electrical Engineering"],
+			"dates": "January 2012",
+			"url": "http://www.montgomerycollege.edu"
 		}
 	],
 	"onlineCourses": [
 		{
-			"title": "Front End Web Developer Certification",
+			"title": "Front-End Web Developer Certification",
 			"school": "Udacity",
-			"dates": 2015,
+			"dates": "August 2015",
 			"url": "http://www.udacity.com"
 		}
 	]
@@ -81,45 +123,43 @@ var projects = {
 		{
 			"title": "Project 1: Mockup Portfolio",
 			"dates": "2014",
-			"description": "This project consisted of mocking a web page. The fonts, characteristics, page layout, etc. had to match almost "
-							+ "exactly as the original. The project implemented the use of the HTML & CSS languages.",
+			"description": "This project consisted of implementing the use of HTML & CSS languages to mock a web page derived from just a .pdf file. "
+							+ "The fonts, characteristics, page layout, etc. had to match almost exactly as the original.",
 			"images": ["images/project1.jpg"],
-			"url": "https://github.com/dalegreene/project1"
+			"url": "https://github.com/dalegreene/Project-1---Mock-Website"
 		},
 		{
 			"title": "Project 2: Interactive Resume",
 			"dates": "2014",
-			"description": "This project consisted of an interactive resume design, which is actually this one that you are currently viewing! "
-							+ "The project implemented the use of the HTML, CSS, JavaScript & jQuery languages. The use of jQuery was heavily "
-							+ "implemented in this project with the purpose of gaining a thorough knowledge of its concepts.",
+			"description": "This project consisted of an interactive resume design with the use of HTML, CSS, JavaScript & jQuery, which is actually "
+							+ "this one that you are currently viewing! The use of jQuery was heavily implemented in this project with the purpose "
+							+ "of gaining a thorough knowledge of its concepts.",
 			"images": ["images/project2.jpg"],
-			"url": "https://github.com/dalegreene/frontend-nanodegree-resume"
+			"url": "https://github.com/dalegreene/Project-2---Interactive-Resume"
 		},
 		{
 			"title": "Project 3: Classic Arcade Game",
 			"dates": "2015",
-			"description": "This project consisted of creating a version of the classic arcade game Frogger, implementing the same basic "
-							+ "concepts and functionalities as the original. The project mandated the use of HTML, CSS, JavaScript & HTML5 Canvas.",
+			"description": "This project consisted of utilizing HTML, CSS, JavaScript & HTML5 Canvas to reinvent a version of the classic arcade game "
+							+ "Frogger, implementing the same basic concepts and functionalities as the original with a few original functionalities.",
 			"images": ["images/project3.png"],
-			"url": "https://github.com/dalegreene/frogger"
+			"url": "https://github.com/dalegreene/Project-3---Classic-Arcade-Game-Clone"
 		},
 		{
 			"title": "Project 4: Website Optimization",
 			"dates": "2015",
-			"description": "This project was to run tests and manipulate a web application with the goal of maximizing the web page's speed. "
-							+ "The project implemented the HTML, CSS, JavaScript & jQuery languages. It also implemented the use of Google's "
-							+ "PageSpeed to test the page's performance.",
+			"description": "This project was conducted using HTML, CSS, JavaScript & jQuery to run tests and manipulate a web application with "
+							+ "the goal of maximizing the web page's speed. Google's PageSpeed was utilized to test the page's performance.",
 			"images": ["images/project4_1.png", "images/project4_2.png", "images/project4_3.png"],
-			"url": "https://github.com/dalegreene/frontend-nanodegree-mobile-portfolio"
+			"url": "https://github.com/dalegreene/Project-4---Website-Performance-Optimization"
 		},
 		{
 			"title": "Project 5: Neighborhood Map",
 			"dates": "2015",
-			"description": "This project consisted of constructing  full page map using Google's API. Functionalities included "
-							+ "accessing & using the APIs of public data sources (i.e. Google, Foursquare, etc.) and collecting "
-							+ "a list of popular places within a neighborhood's boundaries and labeling them with markers on the map. "
-							+ "Also, a search bar needed to be added in order to filter the list of places that populated. This project "
-							+ "involved the use of the HTML, CSS, JavaScript & jQuery languages.",
+			"description": "This project consisted of constructing a full page map using Google's API with the implementation of HTML, CSS, "
+							+ "JavaScript & jQuery. Functionalities included accessing & using the APIs of public data sources (i.e. Google, "
+							+ "Foursquare, etc.) and collecting a list of popular places within a neighborhood's boundaries and labeling them with "
+							+ "markers on the map. Also, a search bar was implemented in order to filter the list of places that populated.",
 			"images": ["images/project5.png"],
 			"url": "https://github.com/dalegreene/Project-5---Neighborhood-Map"
 		},
@@ -127,11 +167,11 @@ var projects = {
 			"title": "Project 6: Feed Reader Testing",
 			"dates": "2015",
 			"description": "This project was performed to test the feeds and functionalities of a web page using the Jasmine tester to "
-							+ "make sure there are no errors. Different functionalities of the web application were tested, such as making "
-							+ "sure feeds are defined and not empty, URLs are valid, menu bars function accurately, and feeds are properly "
-							+ "loaded with different information from its predecessor.",
+							+ "make sure there are no errors in order to successfully run the web application. Different functionalities of "
+							+ "the application were tested, such as making sure feeds are defined and not empty, URLs are valid, menu bars function "
+							+ "accurately, and feeds are properly loaded with different information from its predecessor.",
 			"images": ["images/project6.png"],
-			"url": "https://github.com/dalegreene/frontend-nanodegree-feedreader"
+			"url": "https://github.com/dalegreene/Project-6---Feed-Reader-Testing"
 		}
 	]
 }
@@ -145,7 +185,7 @@ bio.display = function() {
 	$("#header").append(formattedbioPic);
 	var formattedMessage = HTMLWelcomeMsg.replace("%data%", bio.message);
 	$("#header").append(formattedMessage);
-	var HTMLname = [HTMLmobile, HTMLemail, HTMLgithub, HTMLlocation];
+	var HTMLname = [HTMLmobile, HTMLemail, HTMLgithub, HTMLlinkedin, HTMLlocation];
 	var i = 0;
 	for (contact in bio.contacts) {
 		var formattedContact = HTMLname[i].replace("%data%", bio.contacts[contact]);
